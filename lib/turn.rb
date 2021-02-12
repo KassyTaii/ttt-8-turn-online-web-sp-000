1b6c4c04d8bb9cd8051ab16ddfaae1e2328fedcd
Kassandra Guerrero <kassandra-g@hotmail.com>
2021-02-12 20:03:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,34 +17,24 @@ def move(board, index, value="X")
   board[index] = value
 end
 
-
-def valid_move?(board, index)
-  if index.between?(0, 8) && !(position_taken?(board, index))
+def valid_move?(board,index)
+  if index.between? (0,8) && !(position_taken?(board, index))
     return true
   else
     false
   end
 end
 
-
 def position_taken?(board, index)
   if board[index] == " " || board[index] == nil
     taken = false
-elsif board[index] == "X" || board[index] == "O"
-  taken = true
-elsif board[index] == "" || board[index] == false
-  taken = false
-end
+  elsif board[index] == "X" || board[index] == "O"
+    taken = true
+  elsif board[index] == "" || board[index] == false
+    taken = false
+  end
 end
 
 def turn(board)
   puts "Please enter 1-9:"
-  num = gets.chomp
-  index = input_to_index(num)
-  if valid_move?(board, index) == true
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
 end
